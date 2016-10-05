97e126c6f74afcc28e0a6875cbd0bd12cdade578
Neal Gordon <nealagordon@gmail.com>
2016-10-04 20:25:35 -0700

    updated doc webpage

--- doc/ipynb_to_html.py
+++ doc/ipynb_to_html.py
@@ -8,13 +8,12 @@ import os, shutil, subprocess
 
 for ipynb in glob('../mechpy/*.ipynb'):
     #get_ipython().system('jupyter nbconvert --to html ' + ipynb)
-
     fb = os.path.splitext( os.path.basename(ipynb) )[0]
     fbe = os.path.join( 'web',fb+'.html')
-    
     subprocess.call('jupyter nbconvert --to html '+ipynb, shell=True)
-    
-    shutil.move(fb+'.html', fbe)
-    
-    print('URL:"https://nagordon.github.io/mechpy/'+fbe+'"')
 
+print('\n')
+for html in glob('*.html'):    
+    print('URL:"https://nagordon.github.io/mechpy/'+html+'"')
+    shutil.move(html, os.path.join( 'web',html)    )
+print('\n')
