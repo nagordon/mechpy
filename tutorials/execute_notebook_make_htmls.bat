9ba83b31429ac4d4d0d642883f797d0bfae26153
Neal Gordon <nealagordon@gmail.com>
2017-02-21 08:55:30 -0800

    Create execute_notebook_make_htmls.bat

---
+++ tutorials/execute_notebook_make_htmls.bat
@@ -0,0 +1,6 @@
+
+:: I could not figure out how to make a batch script loop over *.ipynb files and run jupyter commands
+:: I should probably just make a python script to do it
+
+jupyter nbconvert --execute this_notebook.ipynb
+
